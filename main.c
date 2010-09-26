52ac09faa8fdd5d25b02f310ca6b2c497b45806d
Håkon Nessjøen <haakon.nessjoen@gmail.com>
2010-09-26 20:51:39 +0200

    GPL'd it

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+    Mac-Telnet - Connect to RouterOS clients via MAC address
+    Copyright (C) 2010, Håkon Nessjøen <haakon.nessjoen@gmail.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 #include <stdio.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
@@ -56,7 +74,7 @@ void handlePacket(unsigned char *data, int data_len) {
 				memcpy(encryptionkey, cpkt.data, cpkt.length);
 
 				md5data[0] = 0;
-				strcpy(md5data+1, "eoidt668");
+				strcpy(md5data+1, "password");
 				strncat(md5data+1, encryptionkey, 16);
 
 				MD5_Init(&c);

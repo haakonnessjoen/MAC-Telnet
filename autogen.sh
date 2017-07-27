8b92b226e385ca6474ea64dabd82deddab204d86
Walter Antolini <antwal@gmail.com>
2017-07-27 12:24:32 +0200

    Update autogen

--- autogen.sh
+++ autogen.sh
@@ -2,12 +2,15 @@
 
 set -e
 
-
+# Clean All
 if [ "$1" = "clean" ]; then
-  rm -f aclocal.m4 configure missing install-sh \
+  rm -f aclocal.m4 compile configure install-sh \
         depcomp ltmain.sh config.guess config.sub \
-        `find . -name Makefile.in` compile
+        `find . -name Makefile.in` compile `find . -name Makefile` \
+        `find . -name *.m4` po/Makevars.template po/POTFILES src/config.h \
+        config.log config.rpath config.status
   rm -rf autom4te.cache
+  rm -rf src/.deps
   exit
 fi
 
@@ -21,3 +24,5 @@ LDFLAGS=${LDFLAGS=-lintl}
 export LDFLAGS
 
 ./configure "$@"
+
+set +e

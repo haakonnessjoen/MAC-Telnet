c4cca64c8e52e78f338cb53a8853a6358d29e17e
Walter Antolini <antwal@gmail.com>
2017-07-27 12:09:48 +0200

    Fixed autogen for OSX, Added autogen clean

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,23 @@
 #! /bin/sh
-aclocal -I m4 \
-&& autoreconf -i \
-&& automake --gnu --add-missing
+
+set -e
+
+
+if [ "$1" = "clean" ]; then
+  rm -f aclocal.m4 configure missing install-sh \
+        depcomp ltmain.sh config.guess config.sub \
+        `find . -name Makefile.in` compile
+  rm -rf autom4te.cache
+  exit
+fi
+
+libtoolize --automake
+aclocal -I m4
+autoreconf -i
+automake --gnu --add-missing
 autoconf
+
+LDFLAGS=${LDFLAGS=-lintl}
+export LDFLAGS
+
+./configure "$@"

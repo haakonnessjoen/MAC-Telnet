47720c3d30170e1a099a319e720270d892eadd69
Walter Antolini <antwal@gmail.com>
2018-06-16 17:32:07 +0200

    CentOS 7 - Fixed RPM build script, CentOS 7 - Fixed RPM SPEC file

--- .docker/centos7.sh
+++ .docker/centos7.sh
@@ -4,12 +4,6 @@
 yum -y clean all
 yum -y clean expire-cache
 
-# TODO: No need - Epel Repo
-rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-
-# TODO: Can remove it
-yum -y install yum-plugin-priorities
-
 yum -y groupinstall 'Development Tools'
 
 gcc --version
@@ -19,21 +13,40 @@ gcc --version
 # Prepare the RPM environment
 mkdir -p /tmp/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 
-# TODO: Macro /etc/rpm/macros.dist
-## dist macros.
-#%centos_ver 7
-#%centos 7
-#%rhel 7
-#%dist .el7
-#%el7 1
+# Macros - /etc/rpm/macros.dist
+cat >> /etc/rpm/macros.dist << EOF
+%dist .centos.el7
+%mactelnet 1
+EOF
 
+# Copy SPEC file
+cp MAC-Telnet/config/mactelnet.spec /tmp/rpmbuild/SPECS
 
-# Build test
+# Get latest version of release
+package_version=`grep Version MAC-Telnet/config/mactelnet.spec | awk '{print $2}'`
 
-# Source repo version
+# Create source archive for RPM Build
 pushd MAC-Telnet
+git archive --prefix=mactelnet-${package_version}/ HEAD | gzip > /tmp/rpmbuild/SOURCES/mactelnet-${package_version}.tar.gz
+popd
 
+# Build RPM
+rpmbuild --define '_topdir /tmp/rpmbuild' -ba -vv /tmp/rpmbuild/SPECS/mactelnet.spec
+
+# After building the RPM, try to install it
+# Fix the lock file error on EL7.  /var/lock is a symlink to /var/run/lock
+mkdir -p /var/run/lock
+
+# Testing packages
+yum localinstall -y /tmp/rpmbuild/RPMS/x86_64/mactelnet-*
+
+
+#
+# TODO: Removed for testing rpm builder
+#
+# Source repo version
+#pushd MAC-Telnet
 # Test build
-./autogen.sh
-make all
+#./autogen.sh
+#make all
 
--- config/mactelnet.spec
+++ config/mactelnet.spec
@@ -4,7 +4,7 @@ Release:  1%{?gitrev:.%{gitrev}git}%{?dist}
 Summary:  Console tools for connecting to, and serving, devices using MikroTik RouterOS MAC-Telnet protocol.
 Epoch:    7
 
-License:  
+License:  GNU
 Group:    Applications/System
 URL:      http://lunatic.no/2010/10/routeros-mac-telnet-application-for-linux-users/
 
@@ -18,4 +18,54 @@ URL:      http://lunatic.no/2010/10/routeros-mac-telnet-application-for-linux-us
 Source0: %{name}-%{version}%{?gitrev:-%{gitrev}}.tar.gz
 
 BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+
+%define	_prefix	/usr/local
+
+%description
+%{summary}
+
+%package daemon
+Group: Applications/System
+Summary: Testing
+
+BuildRequires:  redhat-rpm-config
+
+%description daemon
+%{summary}
+
+
+%package client
+Group: Applications/System
+Summary: Testing
+
+BuildRequires:  redhat-rpm-config
+
+%description client
+%{summary}
+
+
+%prep
+%setup -q
+
+%build
+./autogen.sh
+make all
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%install
+make DESTDIR=$RPM_BUILD_ROOT install
+
+%files client
+%defattr(-,root,root,-)
+%attr(755,root,root) %{_bindir}/*
+%doc %{_prefix}/share/*
+
+%files daemon
+%defattr(-,root,root,-)
+%config(noreplace) %{_prefix}/%{_sysconfdir}/mactelnetd.users
+%attr(755,root,root) %{_sbindir}/*
+%doc %{_prefix}/share/*
+
 

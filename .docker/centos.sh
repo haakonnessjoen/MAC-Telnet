d2334a277b1df51d82b1fe3b20e881721bcf083c
Walter Antolini <antwal@gmail.com>
2018-06-16 13:05:08 +0200

    Added missing docker test script for centos 7

---
+++ .docker/centos.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -xe 
+
+# Clean the yum cache
+yum -y clean all
+yum -y clean expire-cache
+
+# Epel Repo
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+yum -y install yum-plugin-priorities
+
+#yum -y install rpm-build gcc gcc-c++ cmake git tar gzip make autotools
+yum -y groupinstall 'Development Tools'
+
+gcc --version
+
+
+# Source repo version
+cd /home
+git clone --depth=50 --branch=master https://github.com/antwal/MAC-Telnet.git antwal/MAC-Telnet
+
+cd antwal/MAC-Telnet
+
+./autogen.sh
+make all
+
